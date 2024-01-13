7a7c12b91d7eebfc5f3f6e3197dc07dc08809295
Lumaa <nathan@ama49.fr>
2024-01-13 12:00:50 +0100

    Refactored

--- TMC-Widget/TMC_WidgetBundle.swift
+++ TMC-Widget/TMC_WidgetBundle.swift
@@ -6,7 +6,7 @@ import SwiftUI
 @main
 struct TMC_WidgetBundle: WidgetBundle {
     var body: some Widget {
-        TMC_Widget()
+//        TMC_Widget()
         
         TMC_WidgetLiveActivity()
         TMC_LAProgress()
